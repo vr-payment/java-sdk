9edf2032e6532317b70784fff02cb22e6418e928
wallee-deployment-user <technical-systems@wallee.com>
2025-12-01 08:17:32 +0000

    Release 9.2.0

--- src/main/java/com/vrpayment/sdk/auth/Authentication.java
+++ src/main/java/com/vrpayment/sdk/auth/Authentication.java
@@ -19,12 +19,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.vrpayment.sdk.auth;
 
 import com.vrpayment.sdk.Pair;
 
 import java.util.Map;
 import java.util.List;
+
 
 public interface Authentication {
     /**
